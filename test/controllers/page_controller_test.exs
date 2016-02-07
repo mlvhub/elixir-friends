f68bcb55c92b48ac45be3ca4db2aa2e903e26ae8
Miguel Lopez V <mlopezva@akamai.com>
2016-02-07 15:53:00 -0600

    Initial commit

---
+++ test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirFriends.PageControllerTest do
+  use ElixirFriends.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
